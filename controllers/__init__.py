00582844916023516fcb6d22de4d2cbcedc7875a
elf_gzp <741424975@qq.com>
2017-08-20 01:35:17 +0800

    重定向/ url到设置页面url

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from . import main
 from . import config
 from . import banner
 from . import category
---
+++ controllers/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import http
+from odoo.http import request
+
+from odoo.addons.web.controllers.main import Home
+
+class WechatMallHome(Home):
+    @http.route('/', type='http', auth="none")
+    def index(self, s_action=None, db=None, **kw):
+        return http.local_redirect('/web#menu_id={menu_id}&action={action_id}'.format(
+            menu_id=request.env.ref('wechat_mall.wechat_mall_config_settings_menuitem_73').id,
+            action_id=request.env.ref('wechat_mall.wechat_mall_config_settings_action_85').id,
+        ), query=request.params, keep_hash=True)
